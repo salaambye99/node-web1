e09f2c0c4e675971adb12163bf2cbdf6dd9fabfb
hamid <hamid@me.local>
2019-06-15 18:47:35 +0430

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const hbs = require('hbs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('toUpper', (text) => text.toUpperCase());
+
+// console.log(__dirname);
+
+// middleware:
+app.use((req, res, use) => {
+    console.log(`salam!: method: ${req.method} , url: ${req.url}`);
+    use();
+});
+
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageName: 'Home',
+        welcomeMessage: 'Hi dear, welcome to my website.'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageName: 'About us'
+    });
+});
+
+
+app.listen(3000);
+
+
+
+
+
